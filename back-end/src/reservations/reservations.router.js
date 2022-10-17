7e7de04a1a9675338603acbfb7699b828696d83c
Jacob Morris <94742915+Jamorris92@users.noreply.github.com>
2022-10-16 19:44:29 -0700

    Add files via upload

---
+++ back-end/src/reservations/reservations.router.js
@@ -0,0 +1,17 @@
+/**
+ * Defines the router for reservation resources.
+ *
+ * @type {Router}
+ */
+
+const router = require("express").Router();
+const controller = require("./reservations.controller");
+const methodNotAllowed = require('../errors/methodNotAllowed');
+
+router.route('/:reservation_id/:reservation_option').put(controller.update).all(methodNotAllowed)
+
+router.route('/').post(controller.create).get(controller.list).all(methodNotAllowed);
+
+router.route('/:reservation_id').get(controller.read).put(controller.update).all(methodNotAllowed);
+
+module.exports = router;
---
+++ front-end/src/layout/Routes.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
+import Dashboard from "../dashboard/Dashboard";
+import NotFound from "./NotFound";
+import NewReservation from "../Reservations/NewReservation";
+import NewTable from "../Tables/NewTable"
+import Search from "../Search/Search";
+import Edit from "../Edit/Edit";
+import Seat from "../Seat/Seat";
+
+/**
+ * Defines all the routes for the application.
+ *
+ * You will need to make changes to this file.
+ *
+ * @returns {JSX.Element}
+ */
+ //today()
+export default function Routes() {
+  return (
+    <Switch>
+      <Route exact={true} path="/">
+        <Redirect to={"/dashboard"} />
+      </Route>
+      <Route exact={true} path="/reservations">
+        <Redirect to={"/dashboard"} />
+      </Route>
+      <Route exact={true} path="/tables">
+      <Redirect to={"/dashboard"} />
+      </Route>
+      <Route path="/dashboard">
+        <Dashboard />
+      </Route>
+      <Route path="/reservations/new">
+        <NewReservation />
+      </Route>
+      <Route path="/tables/new">
+        <NewTable />
+      </Route>
+      <Route path="/search">
+          <Search />
+      </Route>
+      <Route path="/reservations/:reservation_id/edit">
+        <Edit />
+      </Route>
+      <Route path="/reservations/:reservation_id/seat">
+        <Seat  />
+      </Route>
+      <Route>
+        <NotFound />
+      </Route>
+    </Switch>
+  );
+};
